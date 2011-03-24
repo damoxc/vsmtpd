12cb9603ff3987c28ea7ec71a6c2161b9539d43d
Damien Churchill <damoxc@gmail.com>
2011-03-24 17:34:47 +0000

    improve the test_smtp script

--- test_smtp.py
+++ test_smtp.py
@@ -1,8 +1,14 @@
+import sys
 import smtplib
 
+def output((code, msg)):
+    sys.stdout.write('%s %s\n' % (code, msg))
+    sys.stdout.flush()
+
 smtp = smtplib.SMTP('localhost', 2500)
-print smtp.ehlo('moon.localdomain')
+output(smtp.ehlo('moon.localdomain'))
 print smtp.esmtp_features
-print smtp.mail('Damien Churchill <damoxc@gmail.com>')
-print smtp.rcpt('Damien Churchill <damoxc@damoxc.net>')
-print smtp.data('Subject: Testing\n\nTest')
+output(smtp.mail('Damien Churchill <damoxc@gmail.com>'))
+output(smtp.rcpt('Damien Churchill <damoxc@damoxc.net>'))
+output(smtp.data('Subject: Testing\n\nTest'))
+output(smtp.quit())
