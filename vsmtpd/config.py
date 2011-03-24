06cbe9338b4716690c8a78c6a775408e71967eba
Damien Churchill <damoxc@gmail.com>
2011-03-24 12:29:52 +0000

    move the OrderedDict implementation to util
    Since the OrderedDict may well be used else where move it to util and import
    that to the config module.

--- vsmtpd/config.py
+++ vsmtpd/config.py
@@ -21,17 +21,23 @@
 #
 
 import os
-import sys
 try:
     import cStringIO as StringIO
 except ImportError:
     import StringIO
-import collections
 import ConfigParser
+
+from vsmtpd.util import OrderedDict
 
 CONFIG_DIR = '/etc/vsmtpd'
 
 class ConfigWrapper(object):
+    """
+    Wraps a ConfigParser object and exposes the same API however only
+    for a single section. It's useful if the same config object is
+    shared in multiple places and the section name may be changed
+    later on.
+    """
 
     def __init__(self, config, section):
         self.__config = config
@@ -66,111 +72,6 @@ class ConfigWrapper(object):
 
     def __iter__(self):
         return iter(self.__config)
-
-if sys.version_info < (2, 7):
-    from UserDict import DictMixin
-
-    class OrderedDict(dict, DictMixin):
-
-        def __init__(self, *args, **kwds):
-            if len(args) > 1:
-                raise TypeError('expected at most 1 arguments, got %d' % len(args))
-            try:
-                self.__end
-            except AttributeError:
-                self.clear()
-            self.update(*args, **kwds)
-
-        def clear(self):
-            self.__end = end = []
-            end += [None, end, end]         # sentinel node for doubly linked list
-            self.__map = {}                 # key --> [key, prev, next]
-            dict.clear(self)
-
-        def __setitem__(self, key, value):
-            if key not in self:
-                end = self.__end
-                curr = end[1]
-                curr[2] = end[1] = self.__map[key] = [key, curr, end]
-            dict.__setitem__(self, key, value)
-
-        def __delitem__(self, key):
-            dict.__delitem__(self, key)
-            key, prev, next = self.__map.pop(key)
-            prev[2] = next
-            next[1] = prev
-
-        def __iter__(self):
-            end = self.__end
-            curr = end[2]
-            while curr is not end:
-                yield curr[0]
-                curr = curr[2]
-
-        def __reversed__(self):
-            end = self.__end
-            curr = end[1]
-            while curr is not end:
-                yield curr[0]
-                curr = curr[1]
-
-        def popitem(self, last=True):
-            if not self:
-                raise KeyError('dictionary is empty')
-            if last:
-                key = reversed(self).next()
-            else:
-                key = iter(self).next()
-            value = self.pop(key)
-            return key, value
-
-        def __reduce__(self):
-            items = [[k, self[k]] for k in self]
-            tmp = self.__map, self.__end
-            del self.__map, self.__end
-            inst_dict = vars(self).copy()
-            self.__map, self.__end = tmp
-            if inst_dict:
-                return (self.__class__, (items,), inst_dict)
-            return self.__class__, (items,)
-
-        def keys(self):
-            return list(self)
-
-        setdefault = DictMixin.setdefault
-        update = DictMixin.update
-        pop = DictMixin.pop
-        values = DictMixin.values
-        items = DictMixin.items
-        iterkeys = DictMixin.iterkeys
-        itervalues = DictMixin.itervalues
-        iteritems = DictMixin.iteritems
-
-        def __repr__(self):
-            if not self:
-                return '%s()' % (self.__class__.__name__,)
-            return '%s(%r)' % (self.__class__.__name__, self.items())
-
-        def copy(self):
-            return self.__class__(self)
-
-        @classmethod
-        def fromkeys(cls, iterable, value=None):
-            d = cls()
-            for key in iterable:
-                d[key] = value
-            return d
-
-        def __eq__(self, other):
-            if isinstance(other, OrderedDict):
-                return len(self)==len(other) and self.items() == other.items()
-            return dict.__eq__(self, other)
-
-        def __ne__(self, other):
-            return not self == other
-
-else:
-    OrderedDict = collections.OrderedDict
 
 def _dict_to_defaults(sections):
     fp = StringIO.StringIO()
