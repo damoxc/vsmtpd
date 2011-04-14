8f72c686d81e5d4df0560166821c1e76cc200825
Damien Churchill <damoxc@gmail.com>
2011-04-14 13:48:31 +0100

    add a test testing the command decorator

---
+++ vsmtpd/tests/test_connection.py
@@ -0,0 +1,32 @@
+#
+# vsmtpd/tests/test_connection.py
+#
+# Copyright (C) 2011 Damien Churchill <damoxc@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.    See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.    If not, write to:
+#   The Free Software Foundation, Inc.,
+#   51 Franklin Street, Fifth Floor
+#   Boston, MA    02110-1301, USA.
+#
+
+from vsmtpd.connection import command
+from vsmtpd.tests.common import TestCase
+
+class ConnectionTestCase(TestCase):
+
+    def test_connection_decorator(self):
+        @command
+        def foo():
+            pass
+        self.assertTrue(foo._is_command)
