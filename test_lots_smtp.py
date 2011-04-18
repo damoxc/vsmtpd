77b768c9a1117a4d366c2da29dda4e17a01ad5a1
Damien Churchill <damoxc@gmail.com>
2011-04-19 00:01:22 +0100

    output the number of messages sent

--- test_lots_smtp.py
+++ test_lots_smtp.py
@@ -4,7 +4,10 @@ monkey.patch_all()
 
 import smtplib
 
+sent_count = 0
+
 def send_test_msg():
+    global sent_count
     smtp = smtplib.SMTP('localhost', 2500)
     try:
         smtp.sendmail('test@example.com', 'dave@example.com', """Subject: Testing
@@ -14,6 +17,9 @@ To: <john@example.com>
 This is a test ja?!""")
     except:
         pass
+    smtp.close()
+    sent_count += 1
+    print 'Sent: %d' % sent_count
 
 while True:
     jobs = [gevent.spawn(send_test_msg) for i in xrange(0, 250)]
