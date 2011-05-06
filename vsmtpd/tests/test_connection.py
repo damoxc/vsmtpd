4d1a95b54d55b8c14e05d9a0c7cbe03ffde7ddc3
Damien Churchill <damoxc@gmail.com>
2011-05-06 10:43:47 +0100

    test creating a Connection

--- vsmtpd/tests/test_connection.py
+++ vsmtpd/tests/test_connection.py
@@ -20,13 +20,44 @@
 #   Boston, MA    02110-1301, USA.
 #
 
+from cStringIO import StringIO
 from vsmtpd.connection import command
+from vsmtpd.connection import Connection
 from vsmtpd.tests.common import TestCase
 
+class Socket(object):
+
+    def getsockname(self):
+        return ('127.0.0.1', 25)
+
+    def makefile(self):
+        return StringIO()
+
+class Server(object):
+
+    config = {}
+
 class ConnectionTestCase(TestCase):
+
+    def setUp(self):
+        self.socket = Socket()
+        self.server = Server()
+        self.connection = Connection(self.server, self.socket,
+                                     ('127.0.0.1', 48765))
 
     def test_connection_decorator(self):
         @command
         def foo():
             pass
         self.assertTrue(foo._is_command)
+
+    def test_connection_creation(self):
+        connection = self.connection
+        self.assertEqual(connection.local_ip, '127.0.0.1')
+        self.assertEqual(connection.local_host, 'localhost.localdomain')
+        self.assertEqual(connection.local_port, 25)
+        self.assertEqual(connection.remote_ip, '127.0.0.1')
+        self.assertEqual(connection.remote_host, 'localhost.localdomain')
+        self.assertEqual(connection.remote_port, 48765)
+        self.assertEqual(connection.transaction, None)
+
